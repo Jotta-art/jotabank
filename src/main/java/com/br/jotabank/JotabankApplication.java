47bfb5eef23dfae193b7081686e66c5b216196b0
Jotta-Art <joao.pedro42@outlook.com.br>
2023-04-03 06:47:40 -0300

    initial commit

---
+++ src/main/java/com/br/jotabank/JotabankApplication.java
@@ -0,0 +1,13 @@
+package com.br.jotabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotabankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JotabankApplication.class, args);
+    }
+
+}
